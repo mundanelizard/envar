e31d4c4511b222298d71d6c9e338a6256714f569
Mundane Lizard <mundanelizard@gmail.com>
2023-04-23 07:44:09 +0000

    completed client code to create a new user

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,12 +1,66 @@
 package server
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+var endpoint = "http://localhost:5000/"
+
+func CreateAccount(username, password string) error {
+	if len(username) == 0 {
+		return errors.New("invalid username: expected username to be greater than zero")
+	} else if len(password) < 12 {
+		return errors.New("invalid password: expected password to have a min length of 12")
+	}
+
+	url, err := url.JoinPath(endpoint, "/repos/")
+	if err != nil {
+		return err
+	}
+
+	data := map[string]string {
+		"Username": username,
+		"Password": password,
+	}
+	body, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+	
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	body, err = io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode != 201 {
+		return errors.New(string(body))
+	}
+
+	return nil
+}
 
 func CheckAuthentication() (bool, error) {
 	return true, nil
 }
 
 func CreateNewRepo(name string) (string, error) {
+
 	return fmt.Sprintf("https://localhost:8080/repos/%s", name), nil
 }
 
@@ -18,7 +72,6 @@ func PushCount(repo string) (int, error) {
 
 	return 0, nil
 }
-
 
 type User struct {
 	email string
